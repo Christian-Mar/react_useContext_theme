972fb20e61d11fdc751f18c218ca961ce28bf9a4
Christian <71664957+Christian-Mar@users.noreply.github.com>
2021-12-19 08:20:34 +0100

    added link to Github

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,20 @@
 import { NavLink } from 'react-router-dom';
-import styles from './Header.module.css'
+import styles from './Header.module.css';
+import { FaGithub } from 'react-icons/fa';
 
 const Header = () => {
   return (
 		<nav className={styles.navigation}>
-			<NavLink to='/'>
-				Home
-			</NavLink>
-			<NavLink to='/letters'>
-				Letters
-			</NavLink>
-			<NavLink to='/cijfers'>
-				Cijfers
-			</NavLink>
+			<NavLink to='/'>Home</NavLink>
+			<NavLink to='/letters'>Letters</NavLink>
+			<NavLink to='/cijfers'>Cijfers</NavLink>
+			<a
+				href='https://github.com/Christian-Mar/react_useContext_theme'
+				target='_blank'
+				rel='noopener noreferrer'
+			>
+				<FaGithub />
+			</a>
 		</nav>
 	);
 }
