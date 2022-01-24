91859f769ef9959b63ac42074b1fa63a991d025b
Brecht Arnou <brecht.arnou@gmail.com>
2022-01-24 21:23:22 +0100

    add single task instead of boolean flag

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,107 +1,115 @@
-import { useState, useEffect } from 'react';
-import { collection, query, orderBy, onSnapshot, doc, deleteDoc, updateDoc } from 'firebase/firestore';
-import { db } from '../firebase';
+import { useState, useEffect } from "react";
+import {
+  collection,
+  query,
+  orderBy,
+  onSnapshot,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
+import { db } from "../firebase";
 //import { FaEdit } from 'react-icons/fa';
-import EditTask from './EditTask';
-import styles from './TaskList.module.css';
+import EditTask from "./EditTask";
+import styles from "./TaskList.module.css";
 
 function TaskList() {
-	const [tasks, setTasks] = useState([]);	
-	const [edit, setEdit] = useState(false);
+  const [tasks, setTasks] = useState([]);
+  const [taskToEdit, setTaskToEdit] = useState(null);
 
-	/* Data van Firestore binnenhalen met sortering (laatste komt bovenaan) */
-	useEffect(() => {
-		const taskColRef = query(
-			collection(db, 'tasks'),
-			orderBy('created', 'desc')
-		);
-		onSnapshot(taskColRef, snapshot => {
-			setTasks(
-				snapshot.docs.map(doc => ({
-					id: doc.id,
-					data: doc.data(),
-				}))
-			);
-		});
-	}, []);
+  /* Data van Firestore binnenhalen met sortering (laatste komt bovenaan) */
+  useEffect(() => {
+    const taskColRef = query(
+      collection(db, "tasks"),
+      orderBy("created", "desc")
+    );
+    onSnapshot(taskColRef, (snapshot) => {
+      setTasks(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      );
+    });
+  }, []);
 
-	/* Update van de taak - wisselt true & false als 'completed' */
-	const handleComplete = async (task) => {
-		await updateDoc(doc(db, 'tasks', task.id), {completed: !task.data.completed});
-	};
+  /* Update van de taak - wisselt true & false als 'completed' */
+  const handleComplete = async (task) => {
+    await updateDoc(doc(db, "tasks", task.id), {
+      completed: !task.data.completed,
+    });
+  };
 
-	/* Deleten van een taak op basis van id */	
+  /* Deleten van een taak op basis van id */
   const handleDelete = async (id) => {
-    const taskDocRef = doc(db, 'tasks', id)
-		try {
-			await deleteDoc(taskDocRef);
-		} catch (err) {
-			alert(err);
-		}
-	};
+    const taskDocRef = doc(db, "tasks", id);
+    try {
+      await deleteDoc(taskDocRef);
+    } catch (err) {
+      alert(err);
+    }
+  };
 
-	return (
-		<div>
-			<div>
-				<div className={styles.list}>
-					{tasks.map(task => (					
-						<div
-							className={styles.list_item}
-							id={task.id}
-							key={task.id}
-							completed={task.data.completed.toString()} /* zonder toString() werkt het, maar met rode warning in de console */
-							title={task.data.title}
-							description={task.data.description}
-						>
-								<h4
-									className={styles.list_item_title}
-									style={{
-										textDecoration: task.data.completed
-											? 'line-through'
-											: undefined,
-										color: task.data.completed ? 'darkgray' : undefined,
-									}}
-								>
-									{task.data.title}
-								</h4>
-								<p
-									className={styles.list_item_description}
-									style={{
-										textDecoration: task.data.completed
-											? 'line-through'
-											: undefined,
-										color: task.data.completed ? 'darkgray' : undefined,
-									}}
-								>
-									{task.data.description}
-								</p>
-								<button
-									className={styles.list_item_button}
-									onClick={() => handleComplete(task)}
-								>
-									Bezig/Gedaan
-								</button>
-								<button
-									className={styles.list_item_button}
-									onClick={() => setEdit(true)}
-								>
-									Edit
-								</button>
-								<button
-									className={styles.list_item_button}
-									onClick={() => handleDelete(task.id)}
-								>
-									Verwijder
-								</button>
-								{(edit === true) && <EditTask task={task} setEdit={setEdit} />}
-									
-						</div>
-						
-					))}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div>
+        <div className={styles.list}>
+          {tasks.map((task) => (
+            <div
+              className={styles.list_item}
+              id={task.id}
+              key={task.id}
+              completed={task.data.completed.toString()} /* zonder toString() werkt het, maar met rode warning in de console */
+              title={task.data.title}
+              description={task.data.description}
+            >
+              <h4
+                className={styles.list_item_title}
+                style={{
+                  textDecoration: task.data.completed
+                    ? "line-through"
+                    : undefined,
+                  color: task.data.completed ? "darkgray" : undefined,
+                }}
+              >
+                {task.data.title}
+              </h4>
+              <p
+                className={styles.list_item_description}
+                style={{
+                  textDecoration: task.data.completed
+                    ? "line-through"
+                    : undefined,
+                  color: task.data.completed ? "darkgray" : undefined,
+                }}
+              >
+                {task.data.description}
+              </p>
+              <button
+                className={styles.list_item_button}
+                onClick={() => handleComplete(task)}
+              >
+                Bezig/Gedaan
+              </button>
+              <button
+                className={styles.list_item_button}
+                onClick={() => setTaskToEdit(task)}
+              >
+                Edit
+              </button>
+              <button
+                className={styles.list_item_button}
+                onClick={() => handleDelete(task.id)}
+              >
+                Verwijder
+              </button>
+            </div>
+          ))}
+          <EditTask task={taskToEdit} setTaskToEdit={setTaskToEdit} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default TaskList;
--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-	apiKey: 'REACT_APP_FIREBASE_API_KEY',
-	authDomain: 'sandbox-1b354.firebaseapp.com',
-	projectId: 'sandbox-1b354',
-	storageBucket: 'sandbox-1b354.appspot.com',
-	messagingSenderId: '341455704622',
-	appId: '1:341455704622:web:e84d192214c99a9ddec5bf',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "sandbox-1b354.firebaseapp.com",
+  projectId: "sandbox-1b354",
+  storageBucket: "sandbox-1b354.appspot.com",
+  messagingSenderId: "341455704622",
+  appId: "1:341455704622:web:e84d192214c99a9ddec5bf",
 };
 
 // init firebase app
@@ -16,4 +16,4 @@ const app = initializeApp(firebaseConfig);
 // init services from firebase (enkel een database in Firestore wordt gebruikt)
 const db = getFirestore(app);
 
-export {db}
+export { db };
