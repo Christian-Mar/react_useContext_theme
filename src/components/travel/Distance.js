f91c28b9a9ec1bbbc35ac59c3fc672c4d24f6dda
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-02-01 20:04:41 +0100

    map working

--- src/components/travel/Distance.js
+++ src/components/travel/Distance.js
@@ -9,6 +9,8 @@ const Distance = () => {
   // ref for not always rerendering
   const mapElement = useRef();
   const [map, setMap] = useState({});
+  const [longitude, setLongitude] = useState(3.733333);
+  const [latitude, setLatitude] = useState(51.049999);
 
 // Tom tom documentation
   useEffect(() => {
@@ -16,6 +18,12 @@ const Distance = () => {
     let map = tt.map({
       key: process.env.REACT_APP_TOMTOM_API_KEY,
       container: mapElement.current,
+      stylesVisibility: {
+        trafficIncidents: true,
+        trafficFlow: true,
+      },
+      center: [longitude, latitude],
+      zoom: 12,
     })
     console.log(map)
     setMap(map)
@@ -23,7 +31,7 @@ const Distance = () => {
 
   return (
     <div className={styles.container}>
-    <div ref={mapElement}></div>
+    <div ref={mapElement} className={styles.map}></div>
     </div>
   )
 }
